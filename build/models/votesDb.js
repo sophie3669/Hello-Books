707663f800902941ca7c777d36fa98023a2d9aef
Your Name <you@example.com>
2017-10-23 21:54:39 +0100

    controllers modification

---
+++ build/models/votesDb.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var db = {
+
+    votesDb: [{
+        "bookId": 1,
+        "bookName": "living in faith",
+        "userId": "4",
+        "username": "Damola",
+        "upVote": 10,
+        "downVotes": 20
+
+    }]
+};
+exports.default = db;
+//# sourceMappingURL=votesDb.js.map
