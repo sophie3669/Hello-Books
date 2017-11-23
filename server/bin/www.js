d85f00ea1dc88b8aa39975f2c83ce7b3bb94966f
Your Name <you@example.com>
2017-11-23 15:48:28 +0100

    updating the server

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,8 +1,13 @@
 const http = require('http');
 const app = require('../app'); // The express app we just created
 
+const db = require('../server/models/index');
+db.sequelize.sync().then(() =>{
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
+
+});
