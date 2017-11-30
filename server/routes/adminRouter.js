180a654c99d0611c684825cdf54201cc3964d8ed
Your Name <you@example.com>
2017-11-30 15:48:11 +0100

    updated changes

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -5,9 +5,9 @@ import Helpers from '../middleware/helper';
 const adminRouter = express.Router();
 
 
-adminRouter.post('/api/v1/books', Helpers.adminUserExists, Helpers.isActivefalse, Helpers.authAdmin, AdminContoller.addBook);
+adminRouter.post('/api/v1/books', Helpers.adminUserExists, Helpers.authAdmin, AdminContoller.addBook);
 adminRouter.put(
-  '/api/v1/books/:bookId', Helpers.adminUserExists, Helpers.isActivefalse, Helpers.authAdmin, Helpers.bookExists,
+  '/api/v1/books/:bookId', Helpers.adminUserExists, Helpers.authAdmin, Helpers.bookExists,
   AdminContoller.modifyBook,
 );
 adminRouter.put('/api/v1/users/:userId/borrow/:bookId', Helpers.adminUserExists, Helpers.authAdmin, AdminContoller.acceptBorrowedBooks);
